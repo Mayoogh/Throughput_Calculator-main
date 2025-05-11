0898b99273b290d5cf22e75227f0bdf6a0e17ce8
Mayoogh Girish <mayoogh1997@gmail.com>
2025-05-12 01:04:49 +0530

    Update OutputPart.tsx

--- src/Parts/OutputPart.tsx
+++ src/Parts/OutputPart.tsx
@@ -3,7 +3,7 @@ import { Outputs } from "./Inner/Outputs";
 export const OutputPart = () => {
   return (
     <div className="input-part overflow-x-hidden  !w-screen bg-[#DADDD8] p-2 rounded-md shadow-md flex flex-col items-center justify-start text-[#1C1C1C] h-auto">
-      <h2 className="uppercase font-bold text-xl">Performance Summary</h2>
+      <h2 className="font-bold text-xl">Performance Summary</h2>
       <div className="all-inputs w-full p-2 flex items-center justify-center">
         <Outputs />
       </div>
